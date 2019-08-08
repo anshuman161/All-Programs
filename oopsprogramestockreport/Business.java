e7951f9a6d7d446c9a18f007d91e5ccb95089e20
prema2608 <prema.s2608@gmail.com>
2019-08-08 19:33:34 +0530

    today 8 aug

--- oopsprogramestockreport/Business.java
+++ oopsprogramestockreport/Business.java
@@ -2,20 +2,18 @@ package com.bridgelabz.oopsprogramestockreport;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 
-public class Business {
+public class Business
+{
 
 	public CompanyDetails getStockdetails(CompanyDetails companyDetails, String ShareName, int NoOfShare)
 			throws JsonParseException, JsonMappingException, IOException {
@@ -42,4 +40,41 @@ public class Business {
 		}
 		return companyDetails;
 	}
+	
+	
+	/////////////-----------------------sell
+	
+	
+	
+	public CompanyDetails getStockdetailsforsell(CompanyDetails companyDetails, String sharenametosell, int NoOfShare)
+			throws JsonParseException, JsonMappingException, IOException {
+
+		ObjectMapper objMapper = new ObjectMapper();
+		File f = new File("/home/admin1/Desktop/stockforsell.json");
+		InputStream inptStream = new FileInputStream(f);
+		TypeReference<ArrayList<CompanyDetails>> typeReference = new TypeReference<ArrayList<CompanyDetails>>() {
+		};
+		
+		List<Stock> slist = companyDetails.getStockList();
+	ArrayList<Stock> stockList = objMapper.readValue(inptStream, typeReference);
+	               
+		for (Stock stock2 : slist) 
+		{
+			if (stock2.getSharename().equals(sharenametosell)) 
+			{   
+				stock2.setNoofshare(NoOfShare);
+				int sharetotalAmount=NoOfShare*stock2.getShareprice();
+				stock2.setShareprice(stock2.getShareprice()-sharetotalAmount);
+				slist.add(stock2);
+				companyDetails.setStockList(slist);
+				companyDetails.setAmount(companyDetails.getAmount()+sharetotalAmount);
+			}
+		}
+		return companyDetails;
+		
+		
+		
+		
+	}
+	
 }
---
+++ oopsprogramestockreport/BuySell.java
@@ -0,0 +1,86 @@
+package com.bridgelabz.oopsprogramestockreport;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+public class BuySell 
+{
+	public static void main(String[] args) throws JsonParseException, JsonMappingException, IOException {
+
+		ObjectMapper objMapper = new ObjectMapper();
+		File f = new File("/home/admin1/Desktop/SwitchBuySell.json");
+		InputStream inptStream = new FileInputStream(f);
+		TypeReference<List<CompanyDetails>> typeReference = new TypeReference<List<CompanyDetails>>() {
+		};
+		List<CompanyDetails> modles = objMapper.readValue(inptStream, typeReference);
+		Scanner sc=new Scanner(System.in);
+
+ 		System.out.println("Enter your name");
+        String username=sc.nextLine();
+        
+        System.out.println("What do you want to do...");
+        
+        String request=sc.nextLine();
+        
+        
+        switch (request)
+        {
+		case "buy":
+			System.out.println("Enter Share Name to buy");
+	        String sharename=sc.nextLine(); 		
+	 		System.out.println("Enter number of share....");
+	 		int noofshare=sc.nextInt();
+	 		CompanyDetails companyDetails2 = null ;
+	 		for (CompanyDetails companyDetails : modles) {
+				if(companyDetails.getUsername().equals(username))
+				{
+					Business business = new Business();
+					companyDetails2 = business.getStockdetails(companyDetails, sharename, noofshare);
+					System.out.println(companyDetails2.getUsername()+" "+companyDetails2.getAmount()+" "+companyDetails2.getStockList());
+					
+				}
+			}
+	 		List<CompanyDetails> list=new ArrayList<CompanyDetails>();
+	 		list.add(companyDetails2);
+	 		objMapper.writeValue(f,list);
+			break;
+
+		case "sell":
+			System.out.println("Enter Share Name to sell");
+	        String sharenametosell=sc.nextLine(); 		
+	 		System.out.println("Enter number of share....");
+	 		int noofsharetosell=sc.nextInt();
+	 		
+	 		
+	 		CompanyDetails companyDetails3 = null ;
+	 		for (CompanyDetails companyDetails : modles) {
+				if(companyDetails.getUsername().equals(username))
+				{
+					Business business = new Business();
+					companyDetails3 = business.getStockdetailsforsell(companyDetails, sharenametosell, noofsharetosell);
+					System.out.println(companyDetails3.getUsername()+" "+companyDetails3.getAmount()+" "+companyDetails3.getStockList());	
+				}
+			}
+	 		List<CompanyDetails> listtosell=new ArrayList<CompanyDetails>();
+	 		listtosell.add(companyDetails3);
+	 		objMapper.writeValue(f,listtosell);
+			
+			
+			break;
+
+		default:
+			break;
+		}
+        
+	}
+}
